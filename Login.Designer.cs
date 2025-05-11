c7235de9145c38327d70b07a91d7a33b75b72168
AdeenaZaib <adeenazaib42@gmail.com>
2025-05-11 12:25:29 +0500

    trying query

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -33,11 +33,11 @@
             this.Loginn = new RoundedButton();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox2 = new Components.TextBox();
+            this.pwd = new Components.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new Components.TextBox();
+            this.emailtxt = new Components.TextBox();
             this.translucentRoundedPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -49,18 +49,18 @@
             this.translucentRoundedPanel1.Controls.Add(this.Loginn);
             this.translucentRoundedPanel1.Controls.Add(this.linkLabel1);
             this.translucentRoundedPanel1.Controls.Add(this.label4);
-            this.translucentRoundedPanel1.Controls.Add(this.textBox2);
+            this.translucentRoundedPanel1.Controls.Add(this.pwd);
             this.translucentRoundedPanel1.Controls.Add(this.label2);
             this.translucentRoundedPanel1.Controls.Add(this.label3);
             this.translucentRoundedPanel1.Controls.Add(this.label1);
-            this.translucentRoundedPanel1.Controls.Add(this.textBox1);
+            this.translucentRoundedPanel1.Controls.Add(this.emailtxt);
             this.translucentRoundedPanel1.CornerRadius = 10;
             this.translucentRoundedPanel1.Location = new System.Drawing.Point(216, 12);
             this.translucentRoundedPanel1.Name = "translucentRoundedPanel1";
             this.translucentRoundedPanel1.Size = new System.Drawing.Size(369, 415);
             this.translucentRoundedPanel1.TabIndex = 0;
             // 
-            // Login
+            // Loginn
             // 
             this.Loginn.BackColor = System.Drawing.Color.DarkKhaki;
             this.Loginn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
@@ -68,11 +68,12 @@
             this.Loginn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.Loginn.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
             this.Loginn.Location = new System.Drawing.Point(143, 317);
-            this.Loginn.Name = "Login";
+            this.Loginn.Name = "Loginn";
             this.Loginn.Size = new System.Drawing.Size(102, 36);
             this.Loginn.TabIndex = 6;
             this.Loginn.Text = "LOGIN";
             this.Loginn.UseVisualStyleBackColor = false;
+            this.Loginn.Click += new System.EventHandler(this.Loginn_Click);
             // 
             // linkLabel1
             // 
@@ -99,27 +100,27 @@
             this.label4.TabIndex = 1;
             this.label4.Text = "Don\'t have an account?";
             // 
-            // textBox2
+            // pwd
             // 
-            this.textBox2.BackColor = System.Drawing.SystemColors.Window;
-            this.textBox2.BorderColor = System.Drawing.Color.Transparent;
-            this.textBox2.BorderFocusColor = System.Drawing.Color.HotPink;
-            this.textBox2.BorderRadius = 0;
-            this.textBox2.BorderSize = 2;
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.textBox2.Location = new System.Drawing.Point(60, 261);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
-            this.textBox2.Multiline = false;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Padding = new System.Windows.Forms.Padding(10, 7, 10, 7);
-            this.textBox2.PasswordChar = false;
-            this.textBox2.PlaceholderColor = System.Drawing.Color.DarkGray;
-            this.textBox2.PlaceholderText = "";
-            this.textBox2.Size = new System.Drawing.Size(259, 31);
-            this.textBox2.TabIndex = 5;
-            this.textBox2.Texts = "";
-            this.textBox2.UnderlinedStyle = false;
+            this.pwd.BackColor = System.Drawing.SystemColors.Window;
+            this.pwd.BorderColor = System.Drawing.Color.Transparent;
+            this.pwd.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.pwd.BorderRadius = 0;
+            this.pwd.BorderSize = 2;
+            this.pwd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pwd.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.pwd.Location = new System.Drawing.Point(60, 261);
+            this.pwd.Margin = new System.Windows.Forms.Padding(4);
+            this.pwd.Multiline = false;
+            this.pwd.Name = "pwd";
+            this.pwd.Padding = new System.Windows.Forms.Padding(10, 7, 10, 7);
+            this.pwd.PasswordChar = false;
+            this.pwd.PlaceholderColor = System.Drawing.Color.DarkGray;
+            this.pwd.PlaceholderText = "";
+            this.pwd.Size = new System.Drawing.Size(259, 31);
+            this.pwd.TabIndex = 5;
+            this.pwd.Texts = "";
+            this.pwd.UnderlinedStyle = false;
             // 
             // label2
             // 
@@ -155,29 +156,29 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "LOGIN";
             // 
-            // textBox1
+            // emailtxt
             // 
-            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
-            this.textBox1.BorderColor = System.Drawing.Color.Transparent;
-            this.textBox1.BorderFocusColor = System.Drawing.Color.HotPink;
-            this.textBox1.BorderRadius = 0;
-            this.textBox1.BorderSize = 2;
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.textBox1.Location = new System.Drawing.Point(60, 180);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
-            this.textBox1.Multiline = false;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Padding = new System.Windows.Forms.Padding(10, 7, 10, 7);
-            this.textBox1.PasswordChar = false;
-            this.textBox1.PlaceholderColor = System.Drawing.Color.DarkGray;
-            this.textBox1.PlaceholderText = "";
-            this.textBox1.Size = new System.Drawing.Size(259, 31);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.Texts = "";
-            this.textBox1.UnderlinedStyle = false;
+            this.emailtxt.BackColor = System.Drawing.SystemColors.Window;
+            this.emailtxt.BorderColor = System.Drawing.Color.Transparent;
+            this.emailtxt.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.emailtxt.BorderRadius = 0;
+            this.emailtxt.BorderSize = 2;
+            this.emailtxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.emailtxt.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.emailtxt.Location = new System.Drawing.Point(60, 180);
+            this.emailtxt.Margin = new System.Windows.Forms.Padding(4);
+            this.emailtxt.Multiline = false;
+            this.emailtxt.Name = "emailtxt";
+            this.emailtxt.Padding = new System.Windows.Forms.Padding(10, 7, 10, 7);
+            this.emailtxt.PasswordChar = false;
+            this.emailtxt.PlaceholderColor = System.Drawing.Color.DarkGray;
+            this.emailtxt.PlaceholderText = "";
+            this.emailtxt.Size = new System.Drawing.Size(259, 31);
+            this.emailtxt.TabIndex = 1;
+            this.emailtxt.Texts = "";
+            this.emailtxt.UnderlinedStyle = false;
             // 
-            // Form3
+            // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -186,7 +187,7 @@
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.translucentRoundedPanel1);
-            this.Name = "Form3";
+            this.Name = "Login";
             this.Text = "Form3";
             this.Load += new System.EventHandler(this.Form3_Load);
             this.translucentRoundedPanel1.ResumeLayout(false);
@@ -199,9 +200,9 @@
 
         private Components.TranslucentRoundedPanel translucentRoundedPanel1;
         private System.Windows.Forms.Label label1;
-        private Components.TextBox textBox1;
+        private Components.TextBox emailtxt;
         private System.Windows.Forms.Label label3;
-        private Components.TextBox textBox2;
+        private Components.TextBox pwd;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.LinkLabel linkLabel1;
         private System.Windows.Forms.Label label4;
--- Program.cs
+++ Program.cs
@@ -1,21 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace dbproject
 {
+    public struct UserInfo
+    {
+        public int userid;
+        public string fisrtName;
+        public string lastName;
+        public string email;
+        public string password;
+    }
     internal static class Program
     {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        /// 
+        public static UserInfo CurrentUser;
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            CurrentUser = new UserInfo { userid = 0, fisrtName = "", lastName = "", email = "", password = "" };
             Application.Run(new Login());
         }
     }
